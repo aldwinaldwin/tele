57c605cf91c54a0294a3df792f774e31b3d80a34
aldwinaldwin <aldwinaldwin@users.noreply.github.com>
2020-05-28 15:43:21 +0800

    init

---
+++ tele.py
@@ -0,0 +1,27 @@
+import configparser
+#https://core.telegram.org/api/obtaining_api_id
+from telethon.sync import TelegramClient
+from telethon.tl.types import UpdateEditChannelMessage, UpdateNewChannelMessage
+
+# Reading Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+api_id = config['Telegram']['api_id']
+api_hash = str(config['Telegram']['api_hash'])
+username = config['Telegram']['username']
+
+async def handler(update):
+    if isinstance(update, (UpdateEditChannelMessage, UpdateNewChannelMessage)):
+        if update.message.message:
+            print('-------------------'+str(update.message.date)+'----------------')
+            print(update.message.message)
+            print('------------------------------------------------------')
+
+# Use the client in a `with` block. It calls `start/disconnect` automatically.
+client = TelegramClient(username, api_id, api_hash)
+client.start()
+client.add_event_handler(handler)
+
+print('(Press Ctrl+C to stop this)')
+client.run_until_disconnected()
